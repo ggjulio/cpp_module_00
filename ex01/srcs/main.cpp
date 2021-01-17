a344d697fa74510bb01cec4625cba0980addacd7
ggjulio <juligonz@student.42.fr>
2021-01-17 15:50:26 +0100

    .

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 01:49:38 by juligonz          #+#    #+#             */
-/*   Updated: 2021/01/16 19:08:09 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/01/17 15:49:07 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,28 @@
 #include <fstream>
 #include <strings.h>
 
+struct Contact
+{
+	std::string firstName;
+	std::string lastName;
+	std::string nickName;
+	std::string login;
+	std::string postalAddress;
+	std::string emailAddress;
+	std::string phoneNumber;
+	std::string birthdayDate;
+	std::string favoriteMeal;
+	std::string underwearColor;
+	std::string darkestSecret;
+}
+
 int main()
 {
 	std::string command;
+	Contact contacts[9];
 
+	std::cout << "Shitty phonebook :"
+		<< "Usage:\n" << "\tSEARCH: ??\n" << "\tADD: ??\n" << "\tEXIT\n";
 	while (true)
 	{
 		std::cin >> command;
